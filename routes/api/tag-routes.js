3e0fdf9e66d1435b4a1f48f913df43a9c53b98d1
rroyalty <rroyalty@gmail.com>
2021-03-21 11:49:40 -0400

    asdf

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,7 +43,7 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
-    const tagData = await Tag.update({
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
